af47f2b76566a7296e68617d64c24baba77a1684
Hao Cheng <ch1994@outlook.com>
2020-02-27 15:21:46 +0800

    Action (#2)
    * add tsm

--- src/components/wow.vue
+++ src/components/wow.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="wow">
     <el-row :gutter="0">
-      <el-col>
-        <el-button @click="normal">正常游戏</el-button>
+      <el-col :span="18">
+        <el-input v-model="gamePath" placeholder="The folder of WOW" disabled></el-input>
       </el-col>
-      <el-col>
-        <el-button @click="pjzc">评级战场</el-button>
-      </el-col>
-      <el-col>
-        <el-button @click="weimao">尾锚升级</el-button>
+      <el-col :span="6">
+        <el-button @click="setGamePath">选择wow目录</el-button>
       </el-col>
     </el-row>
 
-    <p>{{msg}}</p>
+    <el-row :gutter="0">
+      <el-button @click="normal">正常游戏</el-button>
+      <el-button @click="pjzc">评级战场</el-button>
+      <el-button @click="weimao">尾锚升级</el-button>
+      <el-button @click="tsm">TSM乱码</el-button>
+    </el-row>
+
+    <p>当前游戏模式为:{{msg}}</p>
+
   </div>
 </template>
 
@@ -22,8 +27,9 @@ import Vue from "vue";
 import lib from "@/lib/lib.ts";
 import electron from "electron";
 import Component from "vue-class-component";
-import fs from "fs";
+import fs from "fs-extra";
 import path from "path";
+import glob from "glob";
 @Component
 export default class wow extends Vue {
   gamePath: string = "D:\\Program Files\\World of Warcraft\\_retail_";
@@ -33,8 +39,25 @@ export default class wow extends Vue {
   wm: string = "weimao";
   pj: string = "pjzc";
   nm: string = "normal";
-  msg: string = "1234";
-
+  _msg: string = "";
+  setGamePath() {
+    electron.remote.dialog.showOpenDialog(
+      {
+        title: "请选择魔兽世界目录",
+        defaultPath: "D:\\Program Files\\World of Warcraft\\_retail_",
+        properties: ["openDirectory"]
+      },
+      (folderNames: string) => {
+        this.gamePath = folderNames[0];
+      }
+    );
+  }
+  get fontPath(): string {
+    return path.join(this.gamePath, "Fonts", "ARHei.ttf");
+  }
+  get tsmPath(): string {
+    return path.join(this.normalAddOnPath, "TradeSkillMaster", "Media");
+  }
   get targetAddOnPath(): string {
     return path.join(this.gamePath, this.addOnPath);
   }
@@ -59,20 +82,41 @@ export default class wow extends Vue {
   get normalWTFPath(): string {
     return this.targetWTFPath + this.nm;
   }
+  get msg(): string {
+    let addon = fs.readlinkSync(this.targetAddOnPath);
+    if (addon.includes("pjzc")) {
+      this._msg = "评级战场";
+    } else if (addon.includes("weimao")) {
+      this._msg = "尾锚升级";
+    } else {
+      this._msg = "正常游戏";
+    }
+    return this._msg;
+  }
+  set msg(message: string) {
+    this._msg = message;
+  }
   weimao() {
-    this.msg = this.targetWTFPath;
+    this.msg = "尾锚升级"
     lib.createSymbolLink(this.weimaoAddOnPath, this.targetAddOnPath);
     lib.createSymbolLink(this.weimaoWTFPath, this.targetWTFPath);
   }
   pjzc() {
-    this.msg = "pjzc";
+    this.msg = "评级战场"
     lib.createSymbolLink(this.pjzcAddOnPath, this.targetAddOnPath);
     lib.createSymbolLink(this.pjzcWTFPath, this.targetWTFPath);
   }
   normal() {
-    this.msg = this.normalAddOnPath;
+    this.msg = "正常游戏"
     lib.createSymbolLink(this.normalAddOnPath, this.targetAddOnPath);
     lib.createSymbolLink(this.normalWTFPath, this.targetWTFPath);
+  }
+  tsm() {
+    let pattern = path.join(this.tsmPath, "*.ttf");
+    let fonts = glob.sync(pattern);
+    for (const font of fonts) {
+      fs.copySync(this.fontPath, font, { overwrite: true });
+    }
   }
 }
 </script>
